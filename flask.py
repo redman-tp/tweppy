dbfdafbd7ff582510cd519f2ddf5374bf35dd359
blade_xo <89949214+bladexo@users.noreply.github.com>
2025-05-29 16:40:12 -0700

    Update flask.py

--- flask.py
+++ flask.py
@@ -9,7 +9,7 @@ def home():
     return "I'm alive!"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8000)
 
 def keep_alive():
     t = threading.Thread(target=run)
